c0ab866375532ddb7793127dc47aef5307eb2437
CodeSandbox <support@codesandbox.io>
2024-09-10 10:31:01 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="dark">hello</div>
+    </>
+  );
 }
 
 export default App;
